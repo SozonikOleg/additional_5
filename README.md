8325a66080a0d8e773d768989c211df4439a6003
OlegSozonik <Sozonik999@gmail.com>
2018-09-30 23:51:28 +0300

    first

--- README.md
+++ README.md
@@ -4,7 +4,11 @@
 
 Implement function `check(str, bracketsConfig)`, that for given brackets sequence will return `true` if it is correct and `false` otherwise
 
+реализовать функцию проверки '(ул. bracketsconfig), которая для заданной последовательности скобок возвращает 'true', если это правильно и false в противном случае
+
 In the second param there is `bracketsConfig` - the array of pairs open-closed brackets. Each subarray includes only 2 elements - opening and closing bracket
+
+во втором парам есть 'bracketsconfig' - массив из пары открытые-закрытые скобки. каждый подмассив содержит только 2 элемента - Открытие и закрытие кронштейна
 
 ```js
 check('()', [['(', ')']]) // -> true
--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    var stack = [];
+  
+    var form = {};
+  
+    var type = {};
+  
+    for (var i = 0; i < bracketsConfig.length; i++) {
+
+      var left = bracketsConfig[i][0];
+      var right = bracketsConfig[i][1];
+      form[left] = right;
+      form[right] = left;
+      type[left] = "opened";
+      type[right] = "closed";
+  
+    }
+  
+    for (var i = 0; i < str.length; i++) {
+      var top = stack.length <= 0 ? null : stack[stack.length - 1];
+      var curr = str[i];
+      if (type[curr] == "opened") {
+        stack.push(curr);
+      }
+      else if (type[curr] == "closed" && curr !== form[curr] && stack.length == 0) {
+        return false;
+      } else if (type[curr] == "closed" && top !== form[curr] && curr !== form[curr]) {
+        return false;
+      }
+        else if (type[curr] == "closed" && top == form[curr]) {
+        stack.pop();
+      } else if (type[curr] == "closed" && stack.length == 0 && curr == form[curr]) {
+        stack.push(curr);
+      } else if (type[curr] == "closed" && stack.length > 0 && top == form[curr]){
+        stack.pop;
+      }else if (type[curr] == "closed" && stack.length > 0 && curr == form[curr] ){
+        stack.push(curr);
+      }
+    }
+    return (stack.length == 0) ? true : false;
+  
+  }
